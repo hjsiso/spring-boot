9e258649ecf88e0e2d885474ca811bb08ab85a34
hector <hjsiso@gmail.com>
2018-10-15 19:54:50 -0500

    step 66

--- src/main/java/com/siso/springboot/app/services/UploadFileServiceImpl.java
+++ src/main/java/com/siso/springboot/app/services/UploadFileServiceImpl.java
@@ -13,6 +13,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.core.io.Resource;
 import org.springframework.core.io.UrlResource;
 import org.springframework.stereotype.Service;
+import org.springframework.util.FileSystemUtils;
 import org.springframework.web.multipart.MultipartFile;
 
 
@@ -70,8 +71,18 @@ public class UploadFileServiceImpl implements IUploadFileService {
 	}
 
 	public Path getPath(String filename) {
-		return Paths.get("uploads").resolve(filename).toAbsolutePath();
+		return Paths.get(UPLOADS_FOLDER).resolve(filename).toAbsolutePath();
 
+	}
+
+	@Override
+	public void deleteAll() {
+		FileSystemUtils .deleteRecursively(Paths.get(UPLOADS_FOLDER).toFile());
+	}
+
+	@Override
+	public void init() throws IOException {
+		Files.createDirectory(Paths.get(UPLOADS_FOLDER));
 	}
 
 }
