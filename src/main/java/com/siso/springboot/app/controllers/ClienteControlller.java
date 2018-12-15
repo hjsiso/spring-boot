ae019c4076605277b409a106809bed9a3be4bf41
hector <hjsiso@gmail.com>
2018-12-15 13:54:52 -0500

    112 - get authenticated user in controller

--- src/main/java/com/siso/springboot/app/controllers/ClienteControlller.java
+++ src/main/java/com/siso/springboot/app/controllers/ClienteControlller.java
@@ -7,6 +7,8 @@ import java.util.Map;
 
 import javax.validation.Valid;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.Resource;
 
@@ -15,6 +17,8 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -36,6 +40,8 @@ import com.siso.springboot.app.util.paginator.PageRender;
 @Controller
 @SessionAttributes("cliente")
 public class ClienteControlller {
+
+	protected final Log logger = LogFactory.getLog(this.getClass());
 
 	@Autowired
 	private IClienteService clienteService;
@@ -75,14 +81,26 @@ public class ClienteControlller {
 	}
 
 	@RequestMapping(value = {"/listar", "/"}, method = RequestMethod.GET)
-	public String listar(@RequestParam(name = "page", defaultValue = "0") int page, Model model) {
+	public String listar(@RequestParam(name = "page", defaultValue = "0") int page, Model model, 
+			Authentication authentication) {
+		
+		if(authentication != null) {
+			logger.info("El usuario logeado es ".concat(authentication.getName()));
+		}
 
+		Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+		
+		if(auth != null) {
+			logger.info("Utilizando forma estatica SecurityContextHolder.getContext().getAuthentication(), El usuario logeado es ".concat(auth.getName()));
+		}
+		
 		Pageable pageRequest = PageRequest.of(page, 5);
 
 		Page<Cliente> clientes = clienteService.findAll(pageRequest);
 
 		PageRender<Cliente> pageRender = new PageRender<Cliente>("/listar", clientes);
 
+		
 		model.addAttribute("titulo", "Listado de clientes");
 		model.addAttribute("clientes", clientes);
 		model.addAttribute("page", pageRender);
