6a82d8557d0f160c03f92d48367b289248619682
hector <hjsiso@gmail.com>
2018-12-16 08:18:53 -0500

    115 - add security with annotations

--- src/main/java/com/siso/springboot/app/SpringSecurityConfig.java
+++ src/main/java/com/siso/springboot/app/SpringSecurityConfig.java
@@ -3,6 +3,7 @@ package com.siso.springboot.app;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.User;
@@ -10,45 +11,35 @@ import org.springframework.security.core.userdetails.User.UserBuilder;
 
 import com.siso.springboot.app.auth.handler.LoginSuccessHandler;
 
+@EnableGlobalMethodSecurity(securedEnabled = true, prePostEnabled = true)
 @Configuration
-public class SpringSecurityConfig extends WebSecurityConfigurerAdapter{
-	
-	
+public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
+
 	@Autowired
 	private LoginSuccessHandler successHandler;
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 
-			http.authorizeRequests()
-			.antMatchers("/", "/css/**","/js**","/images/**","/listar").permitAll()
-			.antMatchers("/ver/**").hasAnyRole("USER")
-			.antMatchers("/uploads/**").hasAnyRole("USER")
-			.antMatchers("/form/**").hasAnyRole("ADMIN")
-			.antMatchers("/eliminar/**").hasAnyRole("ADMIN")
-			.antMatchers("/factura/**").hasAnyRole("ADMIN")
-			.anyRequest().authenticated()
-			.and()
-				.formLogin()
-				.successHandler(successHandler)
-				.loginPage("/login")
-				.permitAll()
-			.and()
-			.logout().permitAll()
-			.and()
-			.exceptionHandling().accessDeniedPage("/error_403");
+		http.authorizeRequests().antMatchers("/", "/css/**", "/js**", "/images/**", "/listar").permitAll()
+				/* .antMatchers("/ver/**").hasAnyRole("USER") */
+				/* .antMatchers("/uploads/**").hasAnyRole("USER") */
+				/* .antMatchers("/form/**").hasAnyRole("ADMIN") */
+				/* .antMatchers("/eliminar/**").hasAnyRole("ADMIN") */
+				/* .antMatchers("/factura/**").hasAnyRole("ADMIN") */
+				.anyRequest().authenticated().and().formLogin().successHandler(successHandler).loginPage("/login")
+				.permitAll().and().logout().permitAll().and().exceptionHandling().accessDeniedPage("/error_403");
 	}
 
 	@Autowired
 	public void configurerGlobal(AuthenticationManagerBuilder build) throws Exception {
-	
+
 		@SuppressWarnings("deprecation")
 		UserBuilder users = User.withDefaultPasswordEncoder();
-		
-		build.inMemoryAuthentication()
-		.withUser(users.username("admin").password("12345").roles("ADMIN","USER"))
-		.withUser(users.username("hector").password("12345").roles("USER"));
-		
+
+		build.inMemoryAuthentication().withUser(users.username("admin").password("12345").roles("ADMIN", "USER"))
+				.withUser(users.username("hector").password("12345").roles("USER"));
+
 	}
 
 }
--- src/main/java/com/siso/springboot/app/controllers/ClienteControlller.java
+++ src/main/java/com/siso/springboot/app/controllers/ClienteControlller.java
@@ -18,6 +18,8 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.annotation.Secured;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
@@ -54,6 +56,8 @@ public class ClienteControlller {
 	@Autowired
 	private IUploadFileService uploadFileService;
 
+	
+	@Secured({"ROLE_USER", "ROLE_ADMIN"})
 	@GetMapping(value = "/uploads/{filename:.+}")
 	public ResponseEntity<Resource> verForo(@PathVariable String filename) {
 
@@ -71,6 +75,7 @@ public class ClienteControlller {
 				.body(recurso);
 	}
 
+	@PreAuthorize("hasAnyRole('ROLE_USER', 'ROLE_ADMIN')")
 	@GetMapping(value = "/ver/{id}")
 	public String ver(@PathVariable(value = "id") Long id, Map<String, Object> model, RedirectAttributes flash) {
 		Cliente cliente = clienteService.fetchByIdWithFacturas(id);
@@ -145,6 +150,7 @@ public class ClienteControlller {
 		return "listar";
 	}
 
+	@Secured("ROLE_ADMIN")
 	@RequestMapping(value = "/form")
 	public String crear(Map<String, Object> model) {
 		Cliente cliente = new Cliente();
@@ -153,6 +159,7 @@ public class ClienteControlller {
 		return "form";
 	}
 
+	@PreAuthorize("hasRole('ROLE_ADMIN')")
 	@RequestMapping(value = "/form/{id}")
 	public String editar(@PathVariable(value = "id") Long id, Map<String, Object> model, RedirectAttributes flash) {
 		Cliente cliente = null;
@@ -172,6 +179,7 @@ public class ClienteControlller {
 		return "form";
 	}
 
+	@Secured("ROLE_ADMIN")
 	@RequestMapping(value = "/form", method = RequestMethod.POST)
 	public String guardar(@Valid Cliente cliente, BindingResult result, Model model,
 			@RequestParam("file") MultipartFile foto, RedirectAttributes flash, SessionStatus status) {
@@ -213,6 +221,7 @@ public class ClienteControlller {
 
 	}
 
+	@Secured("ROLE_ADMIN")
 	@RequestMapping(value = "/eliminar/{id}")
 	public String eliminar(@PathVariable(value = "id") Long id, RedirectAttributes flash) {
 
