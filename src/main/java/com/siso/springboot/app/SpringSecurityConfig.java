b8b2db460ca2259f05d7da80e5e31e85fe08031e
hector <hjsiso@gmail.com>
2018-12-26 09:13:24 -0500

    124 - configue and register JpaUserDetailsService

--- src/main/java/com/siso/springboot/app/SpringSecurityConfig.java
+++ src/main/java/com/siso/springboot/app/SpringSecurityConfig.java
@@ -1,6 +1,5 @@
 package com.siso.springboot.app;
 
-import javax.sql.DataSource;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
@@ -8,11 +7,10 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.User.UserBuilder;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 import com.siso.springboot.app.auth.handler.LoginSuccessHandler;
+import com.siso.springboot.app.models.service.JpaUserDetailsService;
 
 @EnableGlobalMethodSecurity(securedEnabled = true, prePostEnabled = true)
 @Configuration
@@ -21,11 +19,13 @@ public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
 	@Autowired
 	private LoginSuccessHandler successHandler;
 
-	@Autowired
-	private DataSource dataSource;
+
 	
 	@Autowired
 	private BCryptPasswordEncoder passwordEncoder;
+	
+	@Autowired
+	private JpaUserDetailsService userDetailsServices;
 	
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
@@ -43,18 +43,8 @@ public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
 	@Autowired
 	public void configurerGlobal(AuthenticationManagerBuilder build) throws Exception {
 		
-		build.jdbcAuthentication()
-		.dataSource(dataSource)
-		.passwordEncoder(passwordEncoder)
-		.usersByUsernameQuery("select username, password, enabled from users where username=?")
-		.authoritiesByUsernameQuery("select u.username, a.authority from authorities a inner join users u on (a.user_id=u.id) where u.username=?");
-
-		/*@SuppressWarnings("deprecation")
-		UserBuilder users = User.withDefaultPasswordEncoder();
-
-		build.inMemoryAuthentication().withUser(users.username("admin").password("12345").roles("ADMIN", "USER"))
-				.withUser(users.username("hector").password("12345").roles("USER"));*/
-
+		build.userDetailsService(userDetailsServices)
+		.passwordEncoder(passwordEncoder);
 	}
 
 }
